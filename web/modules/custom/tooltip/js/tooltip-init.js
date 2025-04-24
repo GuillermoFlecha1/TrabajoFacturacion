3be8b06421beefad96ea513ad281f70880a99497
ibai <hola@gmail.com>
2025-04-24 15:12:03 +0200

    Tooltip funcional, cuando se hace hover sobre el precio total final, aparece un tooltip con un desglose de como se ha formado ese precio total final

--- web/modules/custom/tooltip/js/tooltip-init.js
+++ web/modules/custom/tooltip/js/tooltip-init.js
@@ -2,12 +2,27 @@
   Drupal.behaviors.invoiceTooltip = {
     attach: function (context) {
       if (typeof tippy === 'function') {
+        // Seleccionamos todos los .invoice-amount dentro de 'context'.
         tippy(context.querySelectorAll('.invoice-amount'), {
           placement: 'top',
           animation: 'shift-away',
-          allowHTML: false
+          allowHTML: false,
         });
       }
     }
   };
 })(Drupal);
+
+/*
+Añadir este codigo al campo de precio total final en "Editar > Reescribir Resultados > Reescribir la salida de este campo con texto personalizado"
+Ahi añadiremos en el cuadro de texto el siguiente código:
+
+
+<span
+  class="invoice-amount"
+  data-tippy-content="Subtotal: €{{ total_importe }}&#10;Impuesto: €{{ total_impuesto }}&#10;% Impuesto: {{ tax_percentage|number_format(2) }}%">
+  €{{ total_final }}
+</span>
+
+
+*/
